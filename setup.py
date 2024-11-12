08a841767a364e530ebad9f186b4e1c6961ac4ab
sctech <gamerselimiko@gmail.com>
2024-11-12 14:26:50 +0300

    fix wrong license

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="simple_structured_markup",
-    version="0.1.0",
+    version="0.1.1",
     description="A lightweight parser for Simple Structured Markup (SSM) files.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -13,7 +13,7 @@ setup(
     python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0 License",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
     include_package_data=True,
